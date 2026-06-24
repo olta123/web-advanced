6d8ac226521f986533435f774d86bfe2fe3e9608
olta123 <oltasadiku1@gmail.com>
2026-06-24 20:50:15 +0200

    final personal project

---
+++ module 26 java/js/main.js
@@ -0,0 +1,43 @@
+console.log("GoodBye")
+
+//var - variabla
+//x - emri i variables
+//5 - vlera e variables
+var x = 5;
+
+// var 123 = 5 - gabim ,nuk lejohet emertimi vetem me numra
+//var *ds = 320 gabim, nuk lejohen shenjat
+
+var a = 50 + 50
+console.log(a)
+
+var b = 20
+var c = 30
+var d = b + c
+console.log(d)
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 100;
+var i = 5;
+var j = 100 / 5;
+console.log(j);
+
+//moduli - mbetja
+var k = 10;
+var i = 4;
+var m = k % 1
+console.log(m);
+
+//rritja per nje numer eshte kjo pjesa ++
+var n = 99;
+n++;
+console.log(n)
+
+//zbritja per nje numer eshte kjo pjesa --
+var o = 101;
+o--;
+console.log(onbeforeprint)
